86f934ac5cf089d0ec6b06cff5e2b53273b437f5
Gabrielle Asante <asante.gabrielle@gmail.com>
2024-06-19 18:25:14 +0100

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import "./styles/resets.css";
+import "./styles/typography.css";
+import "./styles/styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
