b133e341aca07c27965b0d0c678dd199e3150c2b
Gabrielle Asante <asante.gabrielle@gmail.com>
2024-07-31 16:41:02 +0100

    Add hover and active state effects to anchor button and fix anchor button font size

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -15,15 +15,18 @@ export default function Hero() {
         <h1 className="text-5xl leading-normal font-semibold mb-6 text-center">
           Hey, I&#39;m Gabrielle.
         </h1>
-        <p className="text-2xl border-violet-500 border-solid text-violet-500 font-medium mb-2">
+        <p className="text-2xl text-violet-500 font-medium mb-2">
           A Full Stack Developer.
         </p>
         <div className="flex flex-col justify-center items-center mt-5">
-          <button className="rounded-lg border bg-violet-500 py-2.5 px-5 text-white twxt-lg cursor-pointer font-medium mb-4 mt-3">
-            <a href={cvLink} target="_blank" rel="noopener noreferrer">
-              Download CV
-            </a>
-          </button>
+          <a
+            href={cvLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="rounded-lg border bg-violet-500 py-2.5 px-5 text-white text-lg cursor-pointer font-medium mb-4 mt-3 hover:bg-violet-600 active:shadow-md active:translate-y-2"
+          >
+            Download CV
+          </a>
         </div>
       </div>
     </section>
