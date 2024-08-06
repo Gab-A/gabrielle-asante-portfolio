0d48427ce903f8ee0764e61c94e3615f6b732ae5
Gabrielle Asante <asante.gabrielle@gmail.com>
2024-08-06 16:12:45 +0100

    Add styling to the about me section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,27 +1,31 @@
 export default function About() {
   return (
-    <div id="about">
-      <h2>About Me.</h2>
-      <p>
-        In 2023, I uncovered my passion for software engineering while seeking
-        out new career opportunities. This newfound interest allowed me to
-        immerse myself to delve into the world of coding, and explore various
-        programming languages and frameworks.
-      </p>
-      <p>
-        After months later of self-teaching, I decided to join a bootcamp. This
-        provided me with structured learning and hands on experience to help me
-        to continuously develop and evolve as a full-stack developer. This
-        intensive program not only solidified my technical foundation but also
-        enhanced my problem-solving abilities and prepared me for real-world
-        challenges in the tech industry.
-      </p>{" "}
-      <p>
-        With a passion for enhancing user interaction through front-end
-        engineering, I’m keen to connect with peers who have shared interests
-        and explore new opportunities I can where I can both add value and
-        further my career.
-      </p>
+    <div id="about" className="m-5">
+      <h2 className="text-4xl font-medium">
+        About Me
+        <span className="text-violet-400 text-5xl">.</span>
+      </h2>
+      <div className="space-y-6 mt-5 text-[17px]">
+        <p>
+          In 2023, I uncovered my passion for software engineering while seeking
+          out new career opportunities. This newfound interest allowed me to
+          immerse myself to delve into the world of coding.
+        </p>
+        <p>
+          After months later of self-teaching, I decided to join a bootcamp.
+          This provided me with structured learning and hands on experience
+          which helped me to continuously develop and evolve as a full-stack
+          developer. This intensive program not only solidified my technical
+          foundation but also enhanced my problem-solving abilities and prepared
+          me for real-world challenges in the tech industry.
+        </p>{" "}
+        <p>
+          With a passion for enhancing user interaction through front-end
+          engineering, I’m keen to connect with peers who have shared interests
+          and explore new opportunities where I can both add value and further
+          my career.
+        </p>
+      </div>
     </div>
   );
 }
