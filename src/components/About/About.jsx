a36ec13b99194cf491cc1b737b9ba50ce9f16751
Gabrielle Asante <asante.gabrielle@gmail.com>
2024-08-06 15:56:40 +0100

    Add initial text to about me section

---
+++ src/components/About/About.jsx
@@ -0,0 +1,27 @@
+export default function About() {
+  return (
+    <div id="about">
+      <h2>About Me.</h2>
+      <p>
+        In 2023, I uncovered my passion for software engineering while seeking
+        out new career opportunities. This newfound interest allowed me to
+        immerse myself to delve into the world of coding, and explore various
+        programming languages and frameworks.
+      </p>
+      <p>
+        After months later of self-teaching, I decided to join a bootcamp. This
+        provided me with structured learning and hands on experience to help me
+        to continuously develop and evolve as a full-stack developer. This
+        intensive program not only solidified my technical foundation but also
+        enhanced my problem-solving abilities and prepared me for real-world
+        challenges in the tech industry.
+      </p>{" "}
+      <p>
+        With a passion for enhancing user interaction through front-end
+        engineering, I’m keen to connect with peers who have shared interests
+        and explore new opportunities I can where I can both add value and
+        further my career.
+      </p>
+    </div>
+  );
+}
