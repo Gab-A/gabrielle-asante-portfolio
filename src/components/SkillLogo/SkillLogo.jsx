edaf6ebdec4b476375d6b729e671cb29c796c215
Gabrielle Asante <asante.gabrielle@gmail.com>
2024-08-07 16:31:09 +0100

    Add basic structure for skills section and add skill logo component

---
+++ src/components/SkillLogo/SkillLogo.jsx
@@ -0,0 +1,11 @@
+export default function SkillLogo({ skill }) {
+  return (
+    <div className=" flex justify-center items-center bg-violet-50 rounded-full p-4 w-20 h-20 shadow-md">
+      <img
+        src={skill}
+        alt={skill}
+        className="w-8 transition-transform duration-500 ease-in-out transform hover:rotate-[360deg]"
+      />
+    </div>
+  );
+}
---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,11 @@
+export default function Skills() {
+  return (
+    <section>
+      <div className="ml-4 mb-5">
+        <h2 className="text-4xl font-medium mb-5">
+          Skills<span className="text-violet-400 text-5xl">.</span>
+        </h2>
+      </div>
+    </section>
+  );
+}
