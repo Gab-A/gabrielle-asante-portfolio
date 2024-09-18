0adfa4d2f3c73120b005b51f6e7daee458ee15b8
Gabrielle Asante <asante.gabrielle@gmail.com>
2024-09-18 16:22:15 +0100

    Add responsive design for Hero section and SVG background

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,27 +6,31 @@ export default function Hero() {
     "https://docs.google.com/document/d/10D9hdBkLzkorl7hwmGyPE1UYbFYVYGHN/edit?usp=sharing&ouid=117175190293271690444&rtpof=true&sd=true";
 
   return (
-    <section className="bg-violet-50 p-3 rounded-lg m-4">
-      <div className="flex flex-col justify-center items-center">
-        <Lottie
-          animationData={womanWorkingAnimation}
-          className="w-3/4 overflow-hidden z-10 m-auto"
-        />
-        <h1 className="text-5xl leading-normal font-semibold mb-6 text-center">
-          Hey, I&#39;m Gabrielle.
-        </h1>
-        <p className="text-2xl text-violet-500 font-medium mb-2">
-          A Full Stack Developer.
-        </p>
-        <div className="flex flex-col justify-center items-center mt-5">
-          <a
-            href={cvLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="rounded-lg border bg-violet-500 py-2.5 px-5 text-white text-lg cursor-pointer font-medium mb-4 mt-3 hover:bg-violet-600 active:shadow-md active:translate-y-2"
-          >
-            Download CV
-          </a>
+    <section className="relative bg-[url('/src/assets/images/wave-background.svg')] bg-no-repeat m-4 rounded-lg bg-cover bg-center overflow-hidden h-[86vh] md:p-[3rem] md:rounded-none md:m-0 md:h-[50vh] lg:h-screen xl:h-screen">
+      <div className="flex flex-col justify-center h-full -mt-16 items-center md:-mt-5 md:max-w-full md:flex-row lg:-mt-20 lg:justify-evenly xl:space-x-6">
+        <div className="md:order-2 md:flex md:w-[80%] lg:w-[60%]">
+          <Lottie
+            animationData={womanWorkingAnimation}
+            className="w-3/4 overflow-hidden z-10 m-auto md:-mt-5 md:w-[85%] lg:w-[70%] xl:w-[60%] 2xl:w-[50%]"
+          />
+        </div>
+        <div className="flex flex-col text-center gap-y-5 mt-4 md:gap-y-4 lg:gap-y-5">
+          <h1 className="text-[2.3rem] sm:text-[2.6rem] leading-normal font-semibold md:whitespace-nowrap md:mb-0 lg:-mr-20 lg:text-6xl xl:text-[5.2rem]">
+            Hey, I&#39;m Gabrielle.
+          </h1>
+          <p className="text-2xl text-violet-500 font-medium md:mb-0 sm:text-[1.7rem] md:text-[1.9rem] lg:text-[2rem] lg:mt-5 xl:text-4xl">
+            A Full Stack Developer.
+          </p>
+          <div className="flex flex-col justify-center items-center md:mt-6">
+            <a
+              href={cvLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="rounded-lg border-violet-500 bg-violet-500 py-2.5 px-5 text-white text-lg cursor-pointer font-medium  hover:bg-violet-600 active:shadow-md active:translate-y-2 sm:text-xl sm:py-3 2xl:py-5 md:mt-0 lg:text-2xl lg:py-4 lg:px-8"
+            >
+              Download CV
+            </a>
+          </div>
         </div>
       </div>
     </section>
