00918907cc0eee0d996be37cb80c8f8b82950d58
Gabrielle Asante <asante.gabrielle@gmail.com>
2024-09-21 16:41:06 +0100

    Add custom bounce keyframes to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        customBounce: {
+          "0%, 100%": {
+            transform: "translate(0)",
+            "box-shadow": "0px 6px 8px rgba(0, 0, 0, 0.2)", // Starting at the original position
+          },
+          "50%": {
+            transform: "translateY(-20px)",
+            "box-shadow": "0px 3px 2px rgba(0, 0, 0, 0.1)", // Move up by 10px
+          },
+        },
+      },
+      animation: {
+        customBounce: "customBounce 3s ease 0s infinite", // Define the animation duration and loop
+      },
+    },
   },
   plugins: [],
 };
