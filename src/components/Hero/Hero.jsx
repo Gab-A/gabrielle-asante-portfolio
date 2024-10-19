ea8d3b7a37f21956b59c6cf5db98ec66b7cc0580
Gabrielle Asante <asante.gabrielle@gmail.com>
2024-10-19 18:03:40 +0100

    Update README and improve responsive design for hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ export default function Hero() {
 
   return (
     <section className="relative bg-[url('/src/assets/images/wave-background.svg')] md:flex justify-center items-center bg-no-repeat m-4 rounded-lg bg-cover bg-center overflow-hidden h-[86vh] md:p-[3rem] md:rounded-none md:m-0 lg:h-screen xl:h-screen">
-      <div className="flex flex-col justify-center h-full -mt-16 items-center md:-mt-5 md:max-w-full md:flex-row lg:-mt-20 lg:pl-4 lg:justify-evenly min-[1115px]:gap-36">
+      <div className="flex flex-col justify-center h-full -mt-16 items-center md:-mt-5 md:max-w-full md:flex-row lg:-mt-20 lg:pl-4 lg:justify-evenly xl:gap-32 min-[1300px]:pr-20 2xl:pr-8 min-[1600px]:pr-0 2xl:gap-36">
         <div className="md:order-2 md:w-[100%] lg:w-[55%] xl:w-1/2 2xl:w-full">
           <Lottie
             animationData={womanWorkingAnimation}
@@ -16,7 +16,7 @@ export default function Hero() {
           />
         </div>
         <div className="flex flex-col text-center gap-y-5 mt-4 md:gap-y-4 lg:gap-y-2 lg:mr- xl:w-1/2 2xl:gap-y-2">
-          <h1 className="text-[2.3rem] sm:text-[2.6rem] leading-normal font-semibold md:text-[2.85rem] md:whitespace-nowrap md:mb-0 lg:text-[3.9rem] 2xl:text-[5.7rem]">
+          <h1 className="text-[2.3rem] sm:text-[2.6rem] leading-normal font-semibold md:text-[2.85rem] md:whitespace-nowrap md:mb-0 lg:text-[3.9rem] 2xl:text-[5.6rem]">
             Hey, I&#39;m Gabrielle.
           </h1>
           <p className="text-2xl text-violet-500 font-medium md:mb-0 sm:text-[1.7rem] lg:mt-2 2xl:text-4xl">
